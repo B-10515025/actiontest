c65638c30005be862a85fc0f5316fae2a26b7381
B-10515025 <ddt22771508@gmail.com>
2021-06-12 08:01:16 +0800

    gtest cpp

---
+++ gtest.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(GoolgeTest, Install) {
+    ASSERT_TRUE(true);
+}
--- hello.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <opencv2/opencv.hpp>
-int main() {
-    std::cout << "opencv lib test\n";
-    cv::waitKey(0);
-    return 0;
-}
